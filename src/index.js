db22566d3bb0b7d84e90843905f90fcb1b99b8e9
Raul Rotundo <raulrotundo@gmail.com>
2017-08-01 10:56:03 -0300

    Router setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import App from './app/app';
+import Login from './app/login/login';
+import Signup from './app/signup/signup';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path="/" component={App}></Route>
+      <Route exact path="/login" component={Login}></Route>
+      <Route exact path="/signup" component={Signup}></Route>
+    </div>
+  </Router>
+), document.getElementById('root'))
 registerServiceWorker();
