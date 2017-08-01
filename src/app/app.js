8d8eaec34282292e2b5cd1c5dea7e5b8b2ddac59
Raul Rotundo <raulrotundo@gmail.com>
2017-08-01 10:55:34 -0300

    Update app component

--- src/app/app.js
+++ src/app/app.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-md-12">
+            <h2>Welcome to React POC</h2>
+            <p>This POC (Proof of concept) is intended to be use as base or demo for projects with React.</p>
+            <p>
+              <Button href="./login" className="btn rounded-btn">Go to Login Form</Button>
+            </p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
