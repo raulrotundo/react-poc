8f7ec5bfa680a2bd1579d2dba46baee3ada8f5d0
Raul Rotundo <raulrotundo@gmail.com>
2017-08-01 10:53:58 -0300

    Adding signup component

---
+++ src/app/signup/signup.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { FormControl, Button, Image } from 'react-bootstrap';
+import './signup.css';
+import userAvatar from '../../assets/images/logo.png';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="login-page">
+        <div className="row">
+          <div className="col-md-4 push-md-4">
+            <Image src={userAvatar} width="150px" className="user-avatar" />
+            <form noValidate>
+              <div className="form-content">
+                <div className="form-group">
+                  <FormControl type="text" className="form-control input-underline input-lg" id="" placeholder="Full Name" />
+                </div>
+                <div className="form-group">
+                  <FormControl type="text" className="form-control input-underline input-lg" id="" placeholder="Email" />
+                </div>
+                <div className="form-group">
+                  <FormControl type="password" className="form-control input-underline input-lg" id="" placeholder="Password" />
+                </div>
+                <div className="form-group">
+                  <FormControl type="password" className="form-control input-underline input-lg" id="" placeholder="Repeat Password" />
+                </div>
+              </div>
+              <Button className="btn rounded-btn" type="submit">Log in</Button>
+              &nbsp;
+              <Button className="btn rounded-btn">Register</Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
