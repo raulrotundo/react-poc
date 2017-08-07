561e1d00f4525630ac3aecd7272435147cb242d8
Raul Rotundo <raulrotundo@gmail.com>
2017-08-06 22:46:27 -0300

    Adding Switch Routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,  Route } from 'react-router-dom';
+import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom';
 import App from './app/app';
 import Login from './app/login/login';
 import Signup from './app/signup/signup';
@@ -10,12 +10,12 @@ import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render((
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={App}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/signup" component={Signup}></Route>
       <Route exact path="/app" component={Layout}></Route>
-    </div>
+    </Switch>
   </Router>
 ), document.getElementById('root'))
 registerServiceWorker();
