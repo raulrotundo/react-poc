31683aaa6b1793f9da0da6587ed574a636813f11
Brian Hume <bhume@digitalriver.com>
2017-08-09 11:32:17 -0300

    Separate view (jsx) from logic (js)

--- src/app/signup/signup.js
+++ src/app/signup/signup.js
@@ -1,39 +1,9 @@
-import React, { Component } from 'react';
-import { FormControl, Button, Image } from 'react-bootstrap';
+import { Component } from 'react';
 import './signup.css';
-import userAvatar from '../../assets/images/logo.png';
+import template from './signup.jsx';
 
 class Signup extends Component {
-  render() {
-    return (
-      <div className="login-page">
-        <div className="row">
-          <div className="col-md-4 push-md-4">
-            <Image src={userAvatar} width="150px" className="user-avatar" />
-            <form noValidate>
-              <div className="form-content">
-                <div className="form-group">
-                  <FormControl type="text" className="form-control input-underline input-lg" id="" placeholder="Full Name" />
-                </div>
-                <div className="form-group">
-                  <FormControl type="text" className="form-control input-underline input-lg" id="" placeholder="Email" />
-                </div>
-                <div className="form-group">
-                  <FormControl type="password" className="form-control input-underline input-lg" id="" placeholder="Password" />
-                </div>
-                <div className="form-group">
-                  <FormControl type="password" className="form-control input-underline input-lg" id="" placeholder="Repeat Password" />
-                </div>
-              </div>
-              <Button className="btn rounded-btn" type="submit">Log in</Button>
-              &nbsp;
-              <Button className="btn rounded-btn">Register</Button>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  render = template;
 }
 
 export default Signup;
