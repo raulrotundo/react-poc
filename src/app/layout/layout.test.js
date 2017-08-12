936e5aeb627824d5a6ab1bc05f9a6417e860b6e5
Raul Rotundo <raulrotundo@gmail.com>
2017-08-12 12:38:34 -0300

    Fix layout test passing props

--- src/app/layout/layout.test.js
+++ src/app/layout/layout.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Layout from './layout';
-import './layout.css';
 
-it('render Layout page without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Layout />, div);
+describe('Layout', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      match: {
+        url: '/app'
+      }
+    };
+  });
+
+  it('render Layout without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <Layout {...props} />
+      </MemoryRouter>
+      , div);
+  });
 });
