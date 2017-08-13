e09ce0008ad374ec0c8f023dbaddaf6d06a9ffcd
Raul Rotundo <raulrotundo@gmail.com>
2017-08-13 18:53:49 -0300

    Fix signup test MemoryRouter

--- src/app/signup/signup.test.js
+++ src/app/signup/signup.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './signup';
+import { MemoryRouter } from 'react-router-dom';
 import './signup.css';
 
-it('render Login without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Signup />, div);
+describe('Signup', () => {
+  it('render Signup without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <Signup />
+      </MemoryRouter>
+    , div)
+  });
 });
