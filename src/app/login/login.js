6f1dd3210686e8cbce4962d31ac5760db5d6a526
Raul Rotundo <rrotundo@digitalriver.com>
2017-08-18 17:18:46 -0300

    Initial Login form authenticaction

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,29 +1,58 @@
-import React, { Component } from 'react';
-import { FormControl, Button, Image } from 'react-bootstrap';
+import React from 'react';
+import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './login.css';
 import userAvatar from '../../assets/images/logo.png';
+import Authentication from '../shared/services/authentication';
 
-class Login extends Component {
+export default class Login extends Authentication {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    };
+
+    this.processForm = this.processForm.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const field = event.target.name;
+    const loginForm = this.state.loginForm;
+    loginForm[field] = event.target.value;
+
+    this.setState({ loginForm });
+  }
+
+  processForm(event) {
+    // Prevent default action. In this case, action is the form submission event
+    event.preventDefault();
+    super.login(this.state.loginForm);
+  }
+
   render() {
     return (
       <div className="login-page">
         <div className="row">
           <div className="col-md-4 push-md-4">
             <Image src={userAvatar} width="150px" className="user-avatar" />
-            <form noValidate>
+            <form onSubmit={this.processForm} noValidate>
               <div className="form-content">
                 <div className="form-group">
-                  <FormControl type="text" name="username" className="form-control input-underline input-lg" placeholder="Email" />
+                  <input type="text" name="username" onChange={this.handleInputChange} value={this.state.loginForm.username} className="form-control input-underline input-lg" placeholder="Email" />
                 </div>
                 <div className="form-group">
-                  <FormControl type="password" name="password" className="form-control input-underline input-lg" placeholder="Password" />
+                  <input type="password" name="password" onChange={this.handleInputChange} value={this.state.loginForm.password} className="form-control input-underline input-lg" placeholder="Password" />
                 </div>
               </div>
-              <Button className="btn rounded-btn" type="submit">Log in</Button>
+              <button className="btn rounded-btn" type="submit">Log in</button>
               &nbsp;
               <Link to="/signup">
-                <Button className="btn rounded-btn">Register</Button>
+                <button className="btn rounded-btn">Register</button>
               </Link>
             </form>
           </div>
@@ -32,5 +61,3 @@ class Login extends Component {
     );
   }
 }
-
-export default Login;
---
+++ src/app/shared/services/authentication.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class Authentication extends Component {
+  login(loginForm) {
+    console.log('LoginForm:', loginForm);
+  }
+}
