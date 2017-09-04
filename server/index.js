8dfe573650626664070401e31e383fa6ef41b70b
Raul Rotundo <raulrotundo@gmail.com>
2017-09-04 20:24:17 -0300

    Fix problem CORS headers pre-flight

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 });
 app.use('/api', router);
@@ -46,4 +49,4 @@ router.post('/authenticate', (req, res) => {
   }, 1000);
 });
 
-app.listen(port, () => console.log('Running on localhost 3001'));
+app.listen(port, () => console.log('Running on localhost ' + port));
