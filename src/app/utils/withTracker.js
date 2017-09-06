511a2cfbbda4c2cbddd21cdde636106822da11f5
Raul Rotundo <rrotundo@digitalriver.com>
2017-09-06 17:53:54 -0300

    React Router v4 withTracker

---
+++ src/app/utils/withTracker.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import GoogleAnalytics from 'react-ga';
+
+GoogleAnalytics.initialize(process.env.REACT_APP_GA_TRACKING_ID, {
+  'cookieDomain': 'auto',
+  'debug': true
+});
+
+const withTracker = (WrappedComponent) => {
+  const trackPage = (page) => {
+    GoogleAnalytics.set({ page });
+    GoogleAnalytics.pageview(page);
+  };
+
+  const HOC = (props) => {
+    const page = props.location.pathname;
+    trackPage(page);
+
+    return (
+      <WrappedComponent {...props} />
+    );
+  };
+
+  return HOC;
+};
+
+export default withTracker;
