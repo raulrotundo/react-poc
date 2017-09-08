f94ce7e03795f5cd4a5a562f202c4376c7fcd689
Raul Rotundo <rrotundo@digitalriver.com>
2017-09-08 11:42:55 -0300

    Adding toggle menu to the sidebar for multi level

--- src/app/components/layout/sidebar/sidebar.js
+++ src/app/components/layout/sidebar/sidebar.js
@@ -3,18 +3,46 @@ import { Link } from 'react-router-dom';
 import './sidebar.css';
 
 class Sidebar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: ''
+    }
+  }
+
+  addExpandClass(element) {
+    this.setState({ showMenu: element === this.state.showMenu ? '' : element });
+  }
+
   render() {
     return (
       <nav className="sidebar">
         <ul className="list-group">
           <Link to="/app" className="list-group-item">
-            <i className="fa fa-fw fa-dashboard"></i>&nbsp;Dashboard
+            <i className="fa fa-fw fa-dashboard"></i>&nbsp; Dashboard
           </Link>
-          <Link to="/app/blank-page" className="list-group-item">
-            <i className="fa fa-file-o"></i>&nbsp;Blank page
-          </Link>
-        </ul>
-      </nav>
+          <div className="nested-menu">
+            <a className="list-group-item" onClick={() => this.addExpandClass('order')}>
+              <span><i className="fa fa-shopping-bag"></i>&nbsp; Orders</span>
+            </a>
+            <li className={this.state.showMenu === 'order' ? 'nested expand' : 'nested'}>
+              <ul className="submenu">
+                <li>
+                  <Link to="/app/orders/new-order" className="list-group-item">
+                    <span><i className="fa fa-chevron-right"></i>&nbsp; Review Orders</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/app/orders/new-order" className="list-group-item">
+                    <span><i className="fa fa-chevron-right"></i>&nbsp; New Order</span>
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </div>
+        </ul >
+      </nav >
     );
   }
 }
