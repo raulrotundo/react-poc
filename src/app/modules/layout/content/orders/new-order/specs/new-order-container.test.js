943de2e0594801a01e0b558e47655ee2f0416fb1
Raul Rotundo <raulrotundo@gmail.com>
2017-09-18 20:59:01 -0300

    Update newOrderContainer test using redux

--- src/app/modules/layout/content/orders/new-order/specs/new-order-container.test.js
+++ src/app/modules/layout/content/orders/new-order/specs/new-order-container.test.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import NewOrderContainer from '../new-order-container';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 describe('NewOrderContainer', () => {
   it('render NewOrderContainer without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <MemoryRouter>
-        <NewOrderContainer />
-      </MemoryRouter>
-    , div)
+      <Provider store={store}>
+        <MemoryRouter>
+          <NewOrderContainer />
+        </MemoryRouter>
+      </Provider>
+      , div)
   });
 });
