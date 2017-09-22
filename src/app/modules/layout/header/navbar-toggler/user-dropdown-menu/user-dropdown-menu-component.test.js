28a5b4dc95934477aa7bec3d9b82bf81d998c693
Raul Rotundo <raulrotundo@gmail.com>
2017-09-21 21:27:34 -0300

    Initial split user drop-down dumb / smart component

--- src/app/modules/layout/header/navbar-toggler/user-dropdown-menu/user-dropdown-menu-component.test.js
+++ src/app/modules/layout/header/navbar-toggler/user-dropdown-menu/user-dropdown-menu-component.test.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
-import UserDropdownMenu from './user-dropdown-menu';
+import UserDropdownMenuComponent from './user-dropdown-menu-component';
 
-describe('UserDropdownMenu', () => {
-  it('render UserDropdownMenu without crashing', () => {
+describe('UserDropdownMenuComponent', () => {
+  it('render UserDropdownMenuComponent without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <Provider store={store}>
         <MemoryRouter>
-          <UserDropdownMenu />
+          <UserDropdownMenuComponent />
         </MemoryRouter>
       </Provider>
       , div)
