74a26209fb16cc503701c251919b8cba63dc2409
Raul Rotundo <raulrotundo@gmail.com>
2017-09-23 22:02:22 -0300

    Setting NotificationComponent as Dumb Component

--- src/app/modules/layout/header/navbar-toggler/notification/notification-component.js
+++ src/app/modules/layout/header/navbar-toggler/notification/notification-component.js
@@ -1,36 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-class Notification extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
-
-  render() {
-    return (
-      <NavDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle nav caret><i className="fa fa-bell"></i></DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem>Pending Task <span className="badge badge-info">6</span></DropdownItem>
-          <DropdownItem>In queue <span className="badge badge-info"> 13</span></DropdownItem>
-          <DropdownItem>Mail <span className="badge badge-info"> 45</span></DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>View All</DropdownItem>
-        </DropdownMenu>
-      </NavDropdown>
-    );
-  }
+const NotificationComponent = (props) => {
+  const { activeNavbarToggler, setNavbarTogglerDropDown } = props;
+  let activeNavbarTogglerIsOpen = activeNavbarToggler === 'notification' ? true : false;
+  return (
+    <NavDropdown isOpen={activeNavbarTogglerIsOpen} toggle={() => setNavbarTogglerDropDown('notification')}>
+      <DropdownToggle nav caret><i className="fa fa-bell"></i></DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>Pending Task <span className="badge badge-info">6</span></DropdownItem>
+        <DropdownItem>In queue <span className="badge badge-info"> 13</span></DropdownItem>
+        <DropdownItem>Mail <span className="badge badge-info"> 45</span></DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>View All</DropdownItem>
+      </DropdownMenu>
+    </NavDropdown>
+  );
 }
 
-export default Notification;
+export default NotificationComponent;
