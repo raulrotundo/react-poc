efeaa7f16c3fed06337fc604e446b37fb05de672
Raul Rotundo <raulrotundo@gmail.com>
2017-09-23 22:01:48 -0300

    Setting LanguageComponent as Dumb Component

--- src/app/modules/layout/header/navbar-toggler/language/language-component.js
+++ src/app/modules/layout/header/navbar-toggler/language/language-component.js
@@ -1,34 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-class Language extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
-
-  render() {
-    return (
-      <NavDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle nav caret><i className="fa fa-language"></i> Language</DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem>English</DropdownItem>
-          <DropdownItem>French</DropdownItem>
-          <DropdownItem>Urdu</DropdownItem>
-        </DropdownMenu>
-      </NavDropdown>
-    );
-  }
+const LanguageComponent = (props) => {
+  const { activeNavbarToggler, setNavbarTogglerDropDown } = props;
+  let activeNavbarTogglerIsOpen = activeNavbarToggler === 'language' ? true : false;
+  return (
+    <NavDropdown isOpen={activeNavbarTogglerIsOpen} toggle={() => setNavbarTogglerDropDown('language')}>
+      <DropdownToggle nav caret><i className="fa fa-language"></i> Language</DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>English</DropdownItem>
+        <DropdownItem>French</DropdownItem>
+        <DropdownItem>Urdu</DropdownItem>
+      </DropdownMenu>
+    </NavDropdown>
+  );
 }
 
-export default Language;
+export default LanguageComponent;
