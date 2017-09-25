2fb2a2871e5feb307dbc9e2efb3084dfaa562b9d
Raul Rotundo <raulrotundo@gmail.com>
2017-09-24 21:26:05 -0300

    Grouping props by type

--- src/app/modules/layout/header/navbar-toggler/user-dropdown-menu/user-dropdown-menu-component.js
+++ src/app/modules/layout/header/navbar-toggler/user-dropdown-menu/user-dropdown-menu-component.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { NavDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const UserDropdownMenuComponent = (props) => {
-  const { auth, activeNavbarToggler, setNavbarTogglerDropDown, onLogout } = props;
+  const { activeNavbarToggler, setNavbarTogglerDropDown, onLogout } = props;
+  const { isAuthenticated, user } = props.auth;
   let activeNavbarTogglerIsOpen = activeNavbarToggler === 'user-drop-down' ? true : false;
   return (
     <NavDropdown isOpen={activeNavbarTogglerIsOpen} toggle={() => setNavbarTogglerDropDown('user-drop-down')}>
-      <DropdownToggle nav caret><i className="fa fa-user"></i> {auth.isAuthenticated ? auth.user.name : 'Undefined Name'}</DropdownToggle>
+      <DropdownToggle nav caret><i className="fa fa-user"></i> {isAuthenticated ? user.name : 'Undefined Name'}</DropdownToggle>
       <DropdownMenu>
         <DropdownItem><i className="fa fa-fw fa-user"></i> Profile</DropdownItem>
         <DropdownItem><i className="fa fa-fw fa-envelope"></i> Inbox</DropdownItem>
