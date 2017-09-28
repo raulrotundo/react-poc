c27d45bcc0a8c198e6dcca152ca24f119752f179
Raul Rotundo <raulrotundo@gmail.com>
2017-09-27 21:40:55 -0300

    Initial Not found page within App layout

---
+++ src/app/modules/layout/not-found-content/not-found-content-component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NotFoundContentComponent = () => {
+  return (
+    <div className="row">
+      <div className="col-md-12 text-center">
+        <div>
+          <h1>Oops!</h1>
+          <h2>404 Not Found</h2>
+          <div>
+            <p>Sorry, an error has occured, Requested page not found!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundContentComponent;
