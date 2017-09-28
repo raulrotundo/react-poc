3da0da7f14893c5e1369c0711c9afca5bf966c15
Raul Rotundo <raulrotundo@gmail.com>
2017-09-27 22:21:15 -0300

    Fix test

--- src/app/modules/layout/content/orders/new-order/new-order-step4/new-order-step4-component.test.js
+++ src/app/modules/layout/content/orders/new-order/new-order-step4/new-order-step4-component.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-import NewOrderStep2Component from './new-order-step2-component';
+import NewOrderStep4Component from './new-order-step4-component';
 
-describe('NewOrderStep2Component', () => {
-  it('render NewOrderStep2Component without crashing', () => {
+describe('NewOrderStep4Component', () => {
+  it('render NewOrderStep4Component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <MemoryRouter>
-        <NewOrderStep2Component />
+        <NewOrderStep4Component />
       </MemoryRouter>
     , div)
   });
