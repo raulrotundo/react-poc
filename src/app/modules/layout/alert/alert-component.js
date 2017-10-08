1f39f62ca3502ab19f55f807911e852e74104fb8
Raul Rotundo <raulrotundo@gmail.com>
2017-10-08 13:41:48 -0300

    Margin top class added

--- src/app/modules/layout/alert/alert-component.js
+++ src/app/modules/layout/alert/alert-component.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './alert-component.css';
 
 const AlertComponent = (props) => {
   const { type, message } = props;
   return (
-    <div className={`alert alert-${type}`} role="alert">
-      {message}
+    <div className="alert-component">
+      <div className={`alert alert-${type}`} role="alert">
+        {message}
+      </div>
     </div>
   );
 }
