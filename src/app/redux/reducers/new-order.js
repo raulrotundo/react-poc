6debe2ca517a726a2af53a9241d802ac04b40cdc
Raul Rotundo <raulrotundo@gmail.com>
2017-10-29 23:25:12 -0300

    Initial step1 form values (inputTypeahead)

--- src/app/redux/reducers/new-order.js
+++ src/app/redux/reducers/new-order.js
@@ -2,16 +2,24 @@ import * as types from 'constants/actionTypes';
 
 const initialState = {
   form: {
-    orderStep: 1
+    orderStep: 1,
+    step1: {
+      inputTypeahead: {
+        allowNew: false,
+        multiple: false,
+        options: []
+      }
+    }
   }
 };
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case types.SET_NEW_ORDER_STEP:
-      return {
+      return {...state,
         form: {
-          orderStep: action.orderStep
+          orderStep: action.orderStep,
+          step1: state.form.step1
         }
       };
     default: return state;
