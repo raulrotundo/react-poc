75472414fc0efdc29e6eee857e6d81847c91804a
Raul Rotundo <raulrotundo@gmail.com>
2017-11-11 21:41:33 -0300

    New ProductTableItemsOrderedComponent added to the step2 screen

--- src/app/modules/layout/content/orders/new-order/new-order-step2/product-table/product-table-component.js
+++ src/app/modules/layout/content/orders/new-order/new-order-step2/product-table/product-table-component.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductTableRowComponent from './product-table-row-component';
+import ProductTableItemsOrderedComponent from './product-table-items-ordered-component';
 
 const ProductTableComponent = (props) => {
   const {
     products,
     isProductListLoading,
-    addToCart
+    addToCart,
+    cart
   } = props;
   const rows = [];
 
@@ -20,16 +22,19 @@ const ProductTableComponent = (props) => {
   });
 
   return (
-    <table className="table table-sm table-bordered">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>{isProductListLoading ? <tr><td colSpan="3" className="text-center">Loading...</td></tr> : rows}</tbody>
-    </table>
+    <div>
+      <table className="table table-sm table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{isProductListLoading ? <tr><td colSpan="3" className="text-center">Loading...</td></tr> : rows}</tbody>
+      </table>
+      {Object.keys(cart).length === 0 || <ProductTableItemsOrderedComponent cart={cart} />}      
+    </div>
   );
 }
 
