ecb8562108d70b131f05fac0b18268f6ffaeac42
Raul Rotundo <rrotundo@digitalriver.com>
2017-11-20 14:39:57 -0300

    Initial product table row not found component

---
+++ src/app/modules/layout/content/orders/new-order/new-order-step2/product-table/product-table-row-not-found-component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductTableRowNotFoundComponent = () => {
+  return (
+    <tr>
+      <td colSpan={5} className="text-center">No data found.</td>
+    </tr>
+  );
+}
+
+export default ProductTableRowNotFoundComponent;
