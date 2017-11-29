512ca6250e7cc41cf9031804b8082970c07dc45e
Raul Rotundo <rrotundo@digitalriver.com>
2017-11-29 16:54:31 -0300

    Removing links from step top bar component to handle it only from the new pagination component

--- src/app/modules/layout/content/orders/new-order/new-order-steps/new-order-steps-component.js
+++ src/app/modules/layout/content/orders/new-order/new-order-steps/new-order-steps-component.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './new-order-steps-component.css';
 
 const NewOrderStepsComponent = (props) => {
-  const { activeStep, getNewOrderStep, newOrderBaseUrl } = props;
+  const { activeStep } = props;
   return (
     <div className="new-order-step">
       <div className="container col-md-offset-3">
         <div className="panel">
           <div className="step">
-            <Link to={`${newOrderBaseUrl}/step1`}>
-              <button onClick={() => getNewOrderStep(1)} className={"btn btn-" + (activeStep === 1 ? 'primary' : 'default') + " btn-circle"}>1</button>
-            </Link>
+            <button className={"btn btn-" + (activeStep === 1 ? 'primary' : 'default') + " btn-circle"}>1</button>
             <p>Step 1</p>
           </div>
           <div className="step">
-            <Link to={`${newOrderBaseUrl}/step2`}>
-              <button onClick={() => getNewOrderStep(2)} className={"btn btn-" + (activeStep === 2 ? 'primary' : 'default') + " btn-circle"}>2</button>
-            </Link>
+            <button className={"btn btn-" + (activeStep === 2 ? 'primary' : 'default') + " btn-circle"}>2</button>
             <p>Step 2</p>
           </div>
           <div className="step">
-            <Link to={`${newOrderBaseUrl}/step3`}>
-              <button onClick={() => getNewOrderStep(3)} className={"btn btn-" + (activeStep === 3 ? 'primary' : 'default') + " btn-circle"}>3</button>
-            </Link>
+            <button className={"btn btn-" + (activeStep === 3 ? 'primary' : 'default') + " btn-circle"}>3</button>
             <p>Step 3</p>
           </div>
           <div className="step">
-            <Link to={`${newOrderBaseUrl}/step4`}>
-              <button onClick={() => getNewOrderStep(4)} className={"btn btn-" + (activeStep === 4 ? 'primary' : 'default') + " btn-circle"}>4</button>
-            </Link>
+            <button className={"btn btn-" + (activeStep === 4 ? 'primary' : 'default') + " btn-circle"}>4</button>
             <p>Step 4</p>
           </div>
         </div>
