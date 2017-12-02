41bad53888b92f51d5df25cab9c639d409692417
Raul Rotundo <raulrotundo@gmail.com>
2017-12-02 15:40:59 -0300

    Fix test

--- src/app/modules/layout/content/orders/new-order/new-order-step2/new-order-step2-component.test.js
+++ src/app/modules/layout/content/orders/new-order/new-order-step2/new-order-step2-component.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 import NewOrderStep2Component from './new-order-step2-component';
 
 describe('NewOrderStep2Component', () => {
   it('render NewOrderStep2Component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <MemoryRouter>
-        <NewOrderStep2Component />
-      </MemoryRouter>
-    , div)
+      <Provider store={store}>
+        <MemoryRouter>
+          <NewOrderStep2Component />
+        </MemoryRouter>
+      </Provider>
+      , div)
   });
 });
