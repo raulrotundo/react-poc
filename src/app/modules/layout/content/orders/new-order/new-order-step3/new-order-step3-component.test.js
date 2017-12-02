eae0525fd7d0a1c45109a874f5e1b8a894c7f497
Raul Rotundo <raulrotundo@gmail.com>
2017-12-02 15:41:22 -0300

    Fix test step3 component

--- src/app/modules/layout/content/orders/new-order/new-order-step3/new-order-step3-component.test.js
+++ src/app/modules/layout/content/orders/new-order/new-order-step3/new-order-step3-component.test.js
@@ -4,12 +4,22 @@ import { MemoryRouter } from 'react-router-dom';
 import NewOrderStep3Component from './new-order-step3-component';
 
 describe('NewOrderStep3Component', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      cart: {
+        items: []
+      }
+    };
+  });
+
   it('render NewOrderStep3Component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <MemoryRouter>
-        <NewOrderStep3Component />
+        <NewOrderStep3Component {...props} />
       </MemoryRouter>
-    , div)
+      , div)
   });
 });
