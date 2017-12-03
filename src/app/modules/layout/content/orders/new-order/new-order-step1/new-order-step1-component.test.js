b4dfc360618debc223c1e1af8d326fc8fcf90bdb
Raul Rotundo <raulrotundo@gmail.com>
2017-12-03 18:17:46 -0300

    Fix test

--- src/app/modules/layout/content/orders/new-order/new-order-step1/new-order-step1-component.test.js
+++ src/app/modules/layout/content/orders/new-order/new-order-step1/new-order-step1-component.test.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 import NewOrderStep1Component from './new-order-step1-component';
 
 describe('NewOrderStep1Component', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      form: {
+        orderStep: 1,
+        step1: {
+          inputTypeahead: {
+            allowNew: false,
+            isLoading: false,
+            multiple: false,
+            autoFocus: true,
+            options: [],
+            selected: []
+          },
+          isBillingAddressInputsVisible: false
+        }
+      },
+      customerSearch: jest.fn()
+    };
+  });
+
   it('render NewOrderStep1Component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <MemoryRouter>
-        <NewOrderStep1Component />
-      </MemoryRouter>
-    , div)
+      <Provider store={store}>
+        <MemoryRouter>
+          <NewOrderStep1Component {...props} />
+        </MemoryRouter>
+      </Provider>
+      , div)
   });
 });
